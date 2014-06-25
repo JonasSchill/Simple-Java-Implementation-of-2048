3a4ac743a73d0d7b20c52bcbea7d6a3f6008a380
Jonas Schill <jonas.schill@gmail.com>
2014-06-25 13:36:01 -0700

    Made Framework for Board Setup
    made a basic framework for the methods that have to be called when a new state is constructed

--- State.java
+++ State.java
@@ -1,1 +1,27 @@
-//State
+import java.util.ArrayList;
+
+public class State{
+  private ArrayList<ArrayList<Integer>> board;
+  
+  public State(){
+    board = new ArrayList<ArrayList<Integer>>();
+    setUp();
+  }
+  
+  public void setUp(){
+    //fill board with all 0's
+    spawnCell();
+    spawnCell();
+  }
+  
+  public void spawnCell(){
+    int[] cord = getRandomEmptyCord();
+    //put a 2 or 4 in a random spot of board with a zero calls of getRandomEmptyCord
+  }
+  
+  public int[] getRandomEmptyCord(){
+    int[] cord = new int[2];
+    //get a random empty cord
+    return cord;
+  }
+}
