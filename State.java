b46060edee3ff07c1ecd8083417caa860c8ef200
Jonas Schill <jonas.schill@gmail.com>
2014-06-26 12:38:13 -0700

    Update State.java

--- State.java
+++ State.java
@@ -1,48 +1,43 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-public class State{
-  private ArrayList<ArrayList<Integer>> board;
-  
-  public State(){
-    board = new ArrayList<ArrayList<Integer>>();
-    setUp();
-  }
-  
-  public void setUp(){
-    //fill board with all 0's
-    ArrayList<Integer> tempRow;
-	Integer tempCell;
-	for(int i = 0; i < 5; i++){
-		tempRow = new ArrayList<Integer>();
-		for(int j = 0; j < 5; j++){
-			tempCell = new Integer(0);
-			tempRow.add(tempCell);
-		}
-		board.add(tempRow);
+public class state {
+	private ArrayList<ArrayList<Integer>> board;
+	
+	public state(){
+	board = new ArrayList<ArrayList<Integer>>();
+	setUp();
 	}
-    spawnCell();
-    spawnCell();
-  }
-  
-  public void spawnCell(){
-    int[] cord = getRandomEmptyCord();
-    
-    //put a 2 or 4 in a random spot of board with a zero calls of getRandomEmptyCord
-    if(Math.random() < 0.5){
+	
+	public void setUp(){
+		ArrayList<Integer> tempRow;
+		Integer tempCell;
+		for(int i = 0; i < 5; i++){
+			tempRow = new ArrayList<Integer>();
+			for(int j = 0; j < 5; j++){
+				tempCell = new Integer(0);
+				tempRow.add(tempCell);
+			}
+			board.add(tempRow);
+		}
+		spawnCell();
+		spawnCell();
+	}
+	
+	public void spawnCell(){
+		int[] cord = getRandomEmptyCord();
+		if(Math.random() < 0.5){
 			board.get(cord[0]).set(cord[1], 2);
 		} else {
 			board.get(cord[0]).set(cord[1], 4);
 		}
-  }
-  
-  public int[] getRandomEmptyCord(){
-    ArrayList<ArrayList<int[]>> emptyCords = new ArrayList<int[]>();
-    int[] cord;
-    Random randomGenerator = new Random();
-    
-    //get all empty cords and select a random one then return
-    		for(int i = 0; i < 5; i++){
+	}
+	
+	public int[] getRandomEmptyCord(){
+		ArrayList<int[]> emptyCords = new ArrayList<int[]>();
+		int[] tempCord;
+		Random randomGenerator = new Random();
+		for(int i = 0; i < 5; i++){
 			for(int j = 0; j < 5; j++){
 				if(board.get(i).get(j) == 0){
 					tempCord = new int[2];
@@ -53,10 +48,10 @@ public class State{
 			}
 		}
 		return emptyCords.get(randomGenerator.nextInt(emptyCords.size()));
-  }
-  
-  public ArrayList<ArrayList<Integer>> getBoard(){
-    return board;
-  }
-  
+	}
+	
+	public ArrayList<ArrayList<Integer>> getBoard(){
+		return board;
+	}
+
 }
